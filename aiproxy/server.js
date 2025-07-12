5581f817689dcf90bbe324bd41cf8fe031411feb
Nelson Homer Talag <ntalag@redhat.com>
2025-07-12 15:00:16 +0800

    updated code:

--- aiproxy/server.js
+++ aiproxy/server.js
@@ -2,14 +2,21 @@ import express from 'express'
 import axios from 'axios'; 
 import cors from 'cors';
 import { config } from 'dotenv';
+import { error } from 'console';
+import { execa } from "execa";
+import { exitCode } from 'process';
+
+ 
 
 config(); 
 const app = express();
 const port = '8002';
 
+
 app.use(cors());
 app.use(express.json());
 var prompt="";
+var rcaid="";
 app.post('/api/generate', async (req, res) => {
   try {
     const apiKey ='AIzaSyBwHJbpEFvAKikUwTOM0pzTkeAtfK8Fn-8';
@@ -46,6 +53,30 @@ app.post('/api/generate', async (req, res) => {
     res.status(500).json({ error: 'Error generating response:' + error + '\n[prompt: '+ prompt +']' });
   }
 });
+app.post('/api/approve', async (req, res) => {
+    rcaid = req.body.rcaid;
+    // console.log(rcaid); 
+    // console.log(process.env.AIOPSSHPATH+ '/aiops-script.sh'); 
+   
+    //exceute bash script 
+    
+    try {
+     
+
+    
+      const { stdout } = await execa("sh " , ["aiops-script.sh "+ rcaid], { cwd: process.env.AIOPSSHPATH,shell:true } ); 
+      console.log(stdout);
+
+    } catch (err) {
+        console.log(err);
+    }
+    const data = {
+      sendstatus: "1",
+      sendmessage:"ansible script sent to ansible server for record : ["+rcaid+"]"
+    };
+    res.json(data);
+    }
+);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);

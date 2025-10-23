52c18940fde083d608c0778516922e032519b449
Nelson Homer Talag <ntalag@redhat.com>
2025-10-23 11:57:21 +0800

    updated code:

--- AIOPSUI/vite.config.js
+++ AIOPSUI/vite.config.js
@@ -1,8 +1,66 @@
 
+// import path from 'path';
+// import framework7 from 'rollup-plugin-framework7';
+
+// import { createHtmlPlugin } from 'vite-plugin-html';
+
+// process.env.TARGET = process.env.TARGET || 'web';
+// const isCordova = process.env.TARGET === 'cordova';
+// const SRC_DIR = path.resolve(__dirname, './src');
+// const PUBLIC_DIR = path.resolve(__dirname, './public');
+// const BUILD_DIR = path.resolve(
+//   __dirname,
+//   isCordova ? './cordova/www' : './www',
+// );
+// export default async () => {
+
+//   return  {
+//     plugins: [
+//       framework7({ emitCss: false }),
+//       createHtmlPlugin({
+//         minify: false,
+//         inject: {
+//           data: {
+//             TARGET: process.env.TARGET,
+//           },
+//         },
+//       }),
+//     ],
+//     root: SRC_DIR,
+//     base: '',
+//     publicDir: PUBLIC_DIR,
+//     build: {
+//       outDir: BUILD_DIR,
+//       assetsInlineLimit: 0,
+//       emptyOutDir: true,
+//       rollupOptions: {
+//         treeshake: false,
+//       },
+//     },
+//     resolve: {
+//       alias: {
+//         '@': SRC_DIR,
+//       },
+//     },
+//     server: {
+//       host: true,
+//       allowedhosts: ["localhost","aiopsui-aiops.apps.cluster-wkq7q.wkq7q.sandbox2937.opentlc.com"]
+//     },
+//     esbuild: {
+//       jsxFactory: '$jsx',
+//       jsxFragment: '"Fragment"',
+//     },
+//   };
+// }
 import path from 'path';
 import framework7 from 'rollup-plugin-framework7';
 
 import { createHtmlPlugin } from 'vite-plugin-html';
+/* export default defineConfig({
+  server: {
+    cors: false, // Disables CORS headers from Vite's dev server
+  },
+}); */
 
 process.env.TARGET = process.env.TARGET || 'web';
 const isCordova = process.env.TARGET === 'cordova';
@@ -44,7 +102,8 @@ export default async () => {
     },
     server: {
       host: true,
-      allowedhosts: ["localhost","aiopsui-aiops.apps.cluster-wkq7q.wkq7q.sandbox2937.opentlc.com"]
+      cors: false,
+      allowedHosts: ['aiopsui-aiops.apps.cluster-wkq7q.wkq7q.sandbox2937.opentlc.com','localhost']
     },
     esbuild: {
       jsxFactory: '$jsx',

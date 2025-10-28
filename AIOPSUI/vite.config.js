1990e98b826def3e191c97ea0e515d1aa82b5601
Vidhya <vidyavece@gmail.com>
2025-10-28 00:38:21 +0000

    Modify vite.config.js and store.js

--- AIOPSUI/vite.config.js
+++ AIOPSUI/vite.config.js
@@ -103,7 +103,7 @@ export default async () => {
     server: {
       host: true,
       cors: false,
-      allowedHosts: ['aiopsui-aiops.apps.cluster-wkq7q.wkq7q.sandbox2937.opentlc.com']
+      allowedHosts: ['aiopsui-aiops.apps.cluster-zhg5b.zhg5b.sandbox515.opentlc.com']
     },
     esbuild: {
       jsxFactory: '$jsx',

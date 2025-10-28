0ef33ea6bf187777d57da4aa7ce242a79557b577
vidhyab3b <vidyavece@gmail.com>
2025-10-28 13:28:30 +0800

    Update store.js

--- AIOPSUI/src/js/store.js
+++ AIOPSUI/src/js/store.js
@@ -7,8 +7,8 @@ const store = createStore({
     // aiproxybaseurl: "http://localhost:8002",
     // baseurl: "http://localhost:8080", 
     // aiproxybaseurl: "http://localhost:8002",
-    baseurl: `${window.location.protocol}//aiproxy-aiops.apps.cluster-zhg5b.zhg5b.sandbox515.opentlc.com`,
-    aiproxybaseurl: `${window.location.protocol}//aiproxy-aiops.apps.cluster-zhg5b.zhg5b.sandbox515.opentlc.com`,
+    baseurl: "http://aiopsui-aiops.apps.cluster-zhg5b.zhg5b.sandbox515.opentlc.com",
+    aiproxybaseurl: "http://aiopsui-aiops.apps.cluster-zhg5b.zhg5b.sandbox515.opentlc.com",
     products: [
       {
         id: '1',

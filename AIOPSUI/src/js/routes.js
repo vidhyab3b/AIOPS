6ae0d23f6c9c5f9e0806633cdfc1c2f91eb71795
vidhyab3b <vidyavece@gmail.com>
2025-10-28 13:28:52 +0800

    Update routes.js

--- AIOPSUI/src/js/routes.js
+++ AIOPSUI/src/js/routes.js
@@ -1,3 +1,4 @@
+
 import HomePage from '../pages/home.f7';
 import AboutPage from '../pages/about.f7';
 import FormPage from '../pages/form.f7';
@@ -33,10 +34,62 @@ var routes = [
     async: function ({ router, to, resolve }) {
       // App instance
       var app = router.app;
+      //console.log(app.store.state.products);
 
       // Show Preloader
       app.preloader.show();
       
+      //console.log(app.request);
+      //get all data from server 
+      // app.request.get(baseurl + "/my-errors/all", (data)=> {
+      //    data=JSON.parse(data)
+      //    app.data.errorlist=data;
+      //    console.log(app)
+      // });
+
+      
+
+      //call gemini
+      
+      // var gprompt = {
+      //   "contents": [
+      //     {
+      //       "parts": [
+      //         {
+      //           "text": "Nginx service is not running. What should I do Give the major headings alone in json format"
+      //         }
+      //       ]
+      //     }
+      //   ]
+      // }
+      // var gdata;
+      // fetch( "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyBwHJbpEFvAKikUwTOM0pzTkeAtfK8Fn-8 ", {
+      //   method: 'POST',
+      //   headers: {
+      //     'type': 'application/json',
+      //     'Content-Type': 'application/json',
+      //     'origin': '',
+      //     'x-stainless-arch': null,
+      //     'x-stainless-lang': null,
+      //     'x-stainless-os': null,
+      //     'x-stainless-package-version': null,
+      //     'x-stainless-retry-count': null,
+      //     'x-stainless-runtime': null,
+      //     'x-stainless-runtime-version': null,
+      //     'x-stainless-timeout': null
+      //   },
+      //   body:  JSON.stringify(gprompt)
+      // })
+      // .then(res => res.json())
+      // .then((data) => {
+         
+      //     console.log(data);
+      //     gdata=data;
+
+      // })
+
+
+
       // User ID from request
       var userId = to.params.userId;
       
@@ -65,65 +118,88 @@ var routes = [
               url: 'http://forum.framework7.io',
             },
             {
-              title: 'cockpit-tls: gnutils_handshake failed: Fatal alert has been received',
+              title: 'cockpit-tls: gnutils_handshake failed: Fatal alert has been recieved',
               url: 'http://forum.framework7.io',
             },
           ]
         };
 
-        var errorsjson;
-        var errors2json;
-
-        // Call open errors via proxy
-        fetch('/api/my-errors/open', {
-          method: 'GET',
-          headers: {
-             'Content-Type': 'application/json',
-             'Accept': 'application/json'
-          },
-        })
-        .then((res) => { 
-          if (res.ok) return res.json();
-          throw new Error(res.status);
-        })
-        .then((data) => {
-          errorsjson = data;
-
-          // Call closed errors via proxy
-          fetch('/api/my-errors/closed', {
+      var errorsjson;
+      var errors2json;
+      //call open errors
+      fetch( app.store.state.baseurl + "/my-errors/open", {
+        method: 'GET',
+        headers: {
+           'type': 'application/json',
+           'accept': 'application/json',
+           'origin': ''
+        },
+      })
+       .then((res) => { 
+        // console.log(res);
+        
+        if (res.ok) {
+          return res.json();
+        }
+        throw new Error(res.status);
+        
+       })
+       .then((data) => {
+          // console.log(data);
+          errorsjson=data;
+          fetch( app.store.state.baseurl + "/my-errors/closed", {
             method: 'GET',
             headers: {
-               'Content-Type': 'application/json',
-               'Accept': 'application/json'
+               'type': 'application/json',
+               'accept': 'application/json',
+               'origin': ''
             },
           })
-          .then((res) => { 
-            if (res.ok) return res.json();
+           .then((res) => { 
+            // console.log(res);
+            
+            if (res.ok) {
+              return res.json();
+            }
             throw new Error(res.status);
-          })
-          .then((data) => {
-            errors2json = data;
-            console.log(errors2json);
-            console.log(errorsjson);
-
-            // Resolve route to load page
-            resolve(
-              {
-                component: RequestAndLoad,
-              },
-              {
-                props: {
-                  user: user,
-                  errorsjson: errorsjson,
-                  errors2json: errors2json
+            
+           })
+           .then((data) => {
+              // console.log(data);
+              errors2json=data;
+              console.log(errors2json);
+              console.log(errorsjson);
+              resolve(
+                {
+                  component: RequestAndLoad,
+                },
+                {
+                  props: {
+                    user: user,
+                    errorsjson: errorsjson,
+                    errors2json: errors2json
+                  }
                 }
-              }
-            );
+              );
+           })
 
-            // Hide Preloader
-            app.preloader.hide();
-          });
-        });
+
+
+
+
+       })
+
+
+       //Fetch Closed Issues
+    
+
+        // Hide Preloader
+        app.preloader.hide();
+        
+        
+
+        // Resolve route to load page
+        
       }, 1000);
     },
   },
